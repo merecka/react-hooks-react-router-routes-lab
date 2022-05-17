90dded155f6bcc0243c62634dfffcaafead2116b
alex-merecka <amerecka@webcreek.com>
2022-05-17 17:03:22 -0500

    All tests passing.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => {
+        return (
+          <div key={index}>
+            <h1>{movie.title}</h1>
+            <h2>{movie.time}</h2>
+            {
+            <ul>
+              {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}
+            </ul>
+            }
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Movies;
